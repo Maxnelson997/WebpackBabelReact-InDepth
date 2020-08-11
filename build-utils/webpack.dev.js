7c9fb8c6c5534bf5bd3bf4e1271b96a4d3a6c57a
Max <maxnelson997@gmail.com>
2020-08-11 17:14:59 -0600

    implemented DotEnv webpack plugin to replace DefinePlugin -- This prevents sensitive information stored in env variables from being exposed

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,14 +1,19 @@
-const { DefinePlugin } = require("webpack")
+// const { DefinePlugin } = require("webpack")
+
+const DotenvWebpackPlugin = require("dotenv-webpack");
 
 module.exports = {
     mode: 'development',
     devtool: 'eval-source-map',
     plugins: [
-        new DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('development'),
-            }
+        new DotenvWebpackPlugin({
+            path: './.env.development'
         })
+        // new DefinePlugin({
+        //     'process.env': {
+        //         'NODE_ENV': JSON.stringify('development'),
+        //     }
+        // })
     ]
 };
 
--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,13 +1,18 @@
-const { DefinePlugin } = require("webpack");
+// const { DefinePlugin } = require("webpack");
+
+const DotenvWebpackPlugin = require("dotenv-webpack");
 
 module.exports = {
     mode: 'production',
     devtool: 'source-map',
     plugins: [
-        new DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
+        new DotenvWebpackPlugin({
+            path: './.env.production'
         })
+        // new DefinePlugin({
+        //     'process.env': {
+        //         'NODE_ENV': JSON.stringify('production')
+        //     }
+        // })
     ]
 };

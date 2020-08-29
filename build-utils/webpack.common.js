89f111e2a48f65292204dea5c8fa6159b82bb41a
Max <maxnelson997@gmail.com>
2020-08-28 18:17:43 -0600

    added support for fonts with file-loader

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -20,6 +20,18 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: '/src/assets'
+                        }
+                    }
+                ]
             }
         ]
     },
